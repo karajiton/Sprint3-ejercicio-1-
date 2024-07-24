97c33538e12e978f6b47038c267ce1bf1266594e
Jonathan Caballero <jonathancaballero916@gmail.com>
2024-07-24 11:19:52 +0200

    Delete Sprint3/Dependency directory

--- Sprint3/Dependency/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'SalirDeCasa.php';
-
-$escenciales = new Escenciales('cartera','llaves de mi casa',false,true,'huawei');
-$salirDeCasa = new SalirDeCasa($escenciales);
-
-print_r ($salirDeCasa);
